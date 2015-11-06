d5cbc1e6670d6a3797e9930eb75bfdbf0289b00e
Luis <delapenl@gmail.com>
2015-11-06 09:59:16 -0700

    finsihed 00_functions

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,10 +1,49 @@
 module.exports = {
 
   helloWorld: function(){
-    // fill me in
+    return "Hello World";
   },
 
   hello: function(string){
-    // fill me in
+    return "Hello " + string;
+  },
+
+  shout: function(string) {
+    var str = string.toUpperCase();
+    return "Hello " + str;
+  },
+
+  whisper: function(string) {
+    var str = string.toLowerCase();
+    return "Hello " + str;
+  },
+
+  separate: function(string) {
+    var str = string.split('');
+    return str;
+  },
+
+  reverseJoin: function(array) {
+    var str = array.reverse('').join();
+    return str;
+  },
+
+  reverseJoinCompact: function(array) {
+    var str = array.reverse('').join().replace(/[,]/ig,'');
+    return str;
+  },
+
+  backwardsDay: function(string) {
+    var str = string.split('').reverse().join().replace(/[,]/ig,'');
+    return "Hello " + str;
   }
-}
+
+};
+
+
+
+
+
+
+
+
