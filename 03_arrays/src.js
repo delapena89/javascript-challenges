d405b327cd6241f0a97e1fd7374c2296c698b008
Luis <delapenl@gmail.com>
2015-11-06 12:50:31 -0700

    getting there

--- 03_arrays/src.js
+++ 03_arrays/src.js
@@ -1,7 +1,53 @@
 module.exports = {
 
   first: function(array) {
-    // fill in code here
+    if(array === []) {
+      return undefined;
+    } else {
+    return array[0];
+  }
+},
+
+  last: function(array) {
+    var arrayLast = array[array.length - 1];
+    if (array === []) {
+      return undefined;
+    } else {
+    return arrayLast;
+  }
+},
+
+  empty: function(array) {
+    if (array.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  first_n: function(array, number) {
+    if (array.length === 0) {
+      return array;
+    }
+      return array.slice(0, number);
+  },
+
+  last_n: function(array, number) {
+    if (array.length === 0) {
+      return array;
+    }
+    return array.splice(-number);
   }
 
-}
+
+
+};
+
+
+
+
+
+
+
+
+
